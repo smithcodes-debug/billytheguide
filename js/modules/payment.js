716ab19e54db53c5c9cf2159fd48cde5e0677626
smithcodes-debug <giveuporkeepgone@gmail.com>
2026-06-26 22:19:29 +0700

    feftg

--- js/modules/payment.js
+++
@@ -1,9 +0,0 @@
-export function initPayment() {
-  const btn = document.getElementById('paymentButton');
-  if (!btn) return;
-
-  btn.addEventListener('click', () => {
-    console.log('Processing payment...');
-    alert('Payment demo success');
-  });
-}
--- js/pages/general.js
+++
@@ -1,6 +0,0 @@
-import { initGesture } from '../ui/gesture.js';
-
-export function initGeneralPage(pageName) {
-  console.log(pageName + ' page loaded');
-  initGesture();
-}
